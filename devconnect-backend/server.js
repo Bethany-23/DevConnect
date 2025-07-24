0eb7cba1751bd12dfb65635e124a48ca8565d3d4
Bethany-23 <bethanyaddis7@gmail.com>
2025-07-24 11:42:15 +0300

    Add user model and auth routes with JWT

--- devconnect-backend/server.js
+++ devconnect-backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRouter");
 
 dotenv.config();
 connectDB();
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req,res)=>{
     res.send("API is running ...")
