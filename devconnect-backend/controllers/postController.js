848c442d6b1025c51c49172c885da474dc89c0e1
Bethany-23 <bethanyaddis7@gmail.com>
2025-07-28 16:03:59 +0300

    added postController with CRUD

---
+++ devconnect-backend/controllers/postController.js
@@ -0,0 +1,81 @@
+
+const Post = require("../models/Post");
+
+// create new post
+
+exports.createPost = async(req, res) =>{
+    try{
+        const {title, content} = req.body;
+        const post = await Post.create({
+            title,
+            content,
+            author: req.user._id
+        });
+        res.status(201).json(post);
+    }catch(err){
+        res.status(500).json({message: err.message});
+    }
+};
+
+// get all posts 
+
+exports.getPosts = async(req, res) =>{
+    try{
+        const posts = await Post.find().populate("author", "name email");
+        res.json(posts);
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
+};
+
+// get single post by ID 
+
+exports.getPostById = async(req, res) =>{
+    try{
+        const post = await Post.findById(req.params.id).populate("author", "name email");
+        if (!post) return res.status(404).json({message: " Post not found"});
+        res.json(post);
+    }catch(err){
+        res.status(500).json({message: err.message});
+    }
+};
+
+// update post 
+
+exports.updatePost = async(req, res) =>{
+    try{
+        const post = await Post.findById(req.params.id);
+        if (!post) return res.status(404).json({message: "Post not found"});
+
+        // check if user is author
+        if (post.author.toString() !== req.user._id.toString()){
+            return res.status(403).json({message: "Not authorized to update this post"});
+        }
+        post.title = req.body.title || post.title;
+        post.content = req.body.content || post.content;
+
+        const updatedPost= await post.save();
+        res.json(updatedPost);
+    }catch(err){
+        res.status(500).json({message: err.message});
+    }
+};
+
+// Delete post
+
+exports.deletePost= async (req, res) =>{
+    try{
+        const post = await Post.findById(req.params.id)
+        if(!post) return res.status(404).json({message: "Post not found"});
+
+        if(post.author.toString() !== req.user._id.toString()){
+            return res.status(403).json({message: "Not authorized to delete the post"});
+        }
+
+        await post.remove();
+        res.json({message: "Post deleted"})
+
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
+};
